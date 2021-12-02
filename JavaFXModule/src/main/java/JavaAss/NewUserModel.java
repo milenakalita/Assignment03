fb77ae28ab06b7d7302e34eca44ff02a22d5daf6
milena <milenakalita@outlook.com>
2021-12-02 23:16:35 +0000

    Added in try and catch in validateEmail and validatePassword to stop the IllegalArgumentException from breaking the code. However it means that the rejectInvalidPassword and rejectInvalidEmail test methods do not pass anymore.

--- JavaFXModule/src/main/java/JavaAss/NewUserModel.java
+++ JavaFXModule/src/main/java/JavaAss/NewUserModel.java
@@ -6,19 +6,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NewUserModel {
+    public NewUserPresenter presenter;
     private String email = null;
     private String password = null;
+    private boolean message;
 
-    public void setEmail(String email) {
-        if (validateEmail(email)) {
-            this.email = email;
-        }
+    public NewUserModel(NewUserPresenter presenter){
+        this.presenter = presenter;
     }
 
-    public void setPassword(String password){
+    public boolean setEmail(String email) {
+        message = false;
+        if (validateEmail(email)) {
+            this.email = email;
+            message = true;
+        }
+        return message;
+    }
+
+    public boolean setPassword(String password){
+        message = false;
         if (validatePassword(password)){
             this.password = password;
+            message = true;
         }
+        return message;
     }
 
     public String getEmail() {
@@ -30,25 +42,33 @@ public class NewUserModel {
     }
 
     public boolean validateEmail(String email){
-        Pattern emailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = emailRegex.matcher(email);
-        if (matcher.matches()) {
-            return true;
+        try {
+            Pattern emailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = emailRegex.matcher(email);
+            if (matcher.matches()) {
+                return true;
+            } else {
+                throw new IllegalArgumentException("Invalid Email");
+            }
         }
-        else{
-            throw new IllegalArgumentException ("Invalid Email");
+        catch(IllegalArgumentException e) {
+            return false;
         }
     }
 
     public boolean validatePassword(String password){
-        Pattern passRegex = Pattern.compile("^.*(?=.{7,})(?=..*[0-9])(?=.*[A-Z])(?=.*[*^&@!]).*$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = passRegex.matcher(password);
-        if (matcher.matches()) {
-            return true;
+        try {
+            Pattern passRegex = Pattern.compile("^.*(?=.{7,})(?=..*[0-9])(?=.*[A-Z])(?=.*[*^&@!]).*$", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = passRegex.matcher(password);
+            if (matcher.matches()) {
+                return true;
+            } else {
+                throw new IllegalArgumentException("Invalid Password");
+            }
         }
-        else{
-            throw new IllegalArgumentException("Invalid Password");
-        }
+        catch(IllegalArgumentException e) {
+            System.out.println("thrown");
+            return false;
+            }
     }
-
 }
