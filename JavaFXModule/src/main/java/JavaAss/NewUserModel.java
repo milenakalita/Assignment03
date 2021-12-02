ce19f1b8c7596f14368c5558b7627444cf39e7ec
milena <milenakalita@outlook.com>
2021-12-02 18:57:49 +0000

    Created NewUserModel.java and getEmail, validateEmail and setEmail methods. Ran the acceptValidEmail test and changed these methods so that the test returned a green bar.

---
+++ JavaFXModule/src/main/java/JavaAss/NewUserModel.java
@@ -0,0 +1,24 @@
+package JavaAss;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NewUserModel {
+    private String email = null;
+
+    public void setEmail(String email) {
+        if (validateEmail(email)) {
+            this.email = email;
+        }
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public boolean validateEmail(String email){
+        Pattern emailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailRegex.matcher(email);
+        return matcher.matches();
+    }
+}
